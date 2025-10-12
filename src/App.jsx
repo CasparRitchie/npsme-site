37a129e253cf6ccd2b72b474b8f7889b872d5af7
Caspar Ritchie <caspar.ritchie@lapiscine.pro>
2025-10-12 18:16:07 +0200

    correct navbar import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import NavBar from "./Navbar";
+import NavBar from "./components/Navbar";
 
 // Pages
 import NpsMeLanding from "./NpsMeLanding";

956220534fc4929a57a8651b6ab666d7358b1146
Caspar Ritchie <caspar.ritchie@lapiscine.pro>
2025-10-23 13:14:49 +0200

    update blog footer visibility

--- src/routesManifest.js
+++ src/routesManifest.js
@@ -12,8 +12,8 @@ export const ROUTES_MANIFEST = [
 
   // Blogs
   { path: "/blog",                             label: "Blog",                           enabled: true,  inHeader: true,  inFooter: true  },
-  { path: "/blog/ethical-surveys",             label: "Blog: Ethical Surveys",          enabled: true,  inHeader: false, inFooter: true },
-  { path: "/blog/ethics-of-contact-selection", label: "Blog: Contact Selection Ethics", enabled: true,  inHeader: false, inFooter: true },
+  { path: "/blog/ethical-surveys",             label: "Blog: Ethical Surveys",          enabled: true,  inHeader: false, inFooter: false },
+  { path: "/blog/ethics-of-contact-selection", label: "Blog: Contact Selection Ethics", enabled: true,  inHeader: false, inFooter: false },
 
   // Regulatory and information
   { path: "/privacy",                          label: "Privacy",                        enabled: true,  inHeader: false, inFooter: true  },
